390488e7dbed028a8bae86ddbd30c60ecf0b493d
Niels <nielsvdc@users.noreply.github.com>
2015-09-05 01:56:24 +0200

    Added script header information

--- GetDataFromKnmi.ps1
+++ GetDataFromKnmi.ps1
@@ -1,6 +1,27 @@
+###################################################################################
+# NAME:   GetDataFromKnmi.ps1
+# AUTHOR: Niels van de Coevering
+# DATE:   05 September 2015
+#
+# COMMENTS: This script will use a webrequest post action to retrieve Dutch weather
+# data from the Royal National Weather Institute KNMI website and use the getdata
+# service for daily and hourly statistics per weather station.
+#
+# This script is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###################################################################################
+
 $outputPath = "E:\Data\";
-$urlDay  = "http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi";
-$urlHour = "http://projects.knmi.nl/klimatologie/uurgegevens/getdata_uur.cgi";
 $startYear = 2005;
 
 ###################################################################################
@@ -32,6 +53,8 @@ function DoWebRequest($url, $params, $outputFile)
     write-host "Finished export file: $outputFile";
 }
 
+$urlDay  = "http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi";
+$urlHour = "http://projects.knmi.nl/klimatologie/uurgegevens/getdata_uur.cgi";
 $curYear = (Get-Date).Year;
 $prevYear = $null;
 if ((Get-Date).Month -eq 1 -and (Get-Date).Day -le 3) { $prevYear = $curYear-1; } # On first 3 days of year also get data from last year
@@ -47,7 +70,7 @@ for ($year=$startYear;$year -le $curYear;$year++)
     # http://projects.knmi.nl/klimatologie/daggegevens/selectie.cgi
     # http://projects.knmi.nl/klimatologie/uurgegevens/selectie.cgi
     $paramsDay = "stns=ALL&vars=ALL&start=$year 0101&end=$year 3112" -Replace " ", "";
-    $paramsHour = "stns=ALL&vars=TEMP:SUNR:PRCP:VICL&start=$year 010101&end= $year 311224" -Replace " ","";
+    $paramsHour = "stns=ALL&vars=ALL&start=$year 010101&end= $year 311224" -Replace " ","";
     
     # If files do not exist for the year, create the files
     if (-not (Test-Path $fileDay))
